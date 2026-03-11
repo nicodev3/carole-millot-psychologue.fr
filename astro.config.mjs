6e2332a1261ac1d2786c15740a9a14a87dd5096e
Nicolas DEVAUX <ndevaux@gmail.com>
2026-03-12 00:01:01 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://carole-millot-psychologue.fr",
   integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,6 @@
 export const site = {
   url: "https://carole-millot-psychologue.fr",
+  id: "https://carole-millot-psychologue.fr/#cabinet",
   name: "Carole Millot",
   profession: "psychologue",
   title: "Carole Millot, psychologue Paris 9",
@@ -7,6 +8,18 @@ export const site = {
   phoneHref: "tel:+33699468879",
   addressLine1: "35 rue Notre Dame de Lorette",
   addressLine2: "75009 Paris",
+  streetAddress: "35 rue Notre Dame de Lorette",
+  postalCode: "75009",
+  city: "Paris",
+  region: "Île-de-France",
+  country: "FR",
+  neighborhood: "Paris 9e",
+  latitude: 48.87913,
+  longitude: 2.3365,
+  mapUrl:
+    "https://www.openstreetmap.org/?mlat=48.87915&mlon=2.33620#map=18/48.87915/2.33620",
+  image: "/img/carole-millot.svg",
+  logo: "/img/logo.svg",
   adeli: "759369069",
   siret: "51502184800012",
 };
